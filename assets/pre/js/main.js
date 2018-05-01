7af70314b88687c903f083a7f374aea5d6b23fe0
Peter Lee <peterlee0127@gmail.com>
2018-05-01 13:37:42 +0800

    update

--- assets/pre/js/main.js
+++ assets/pre/js/main.js
@@ -1,5 +1,12 @@
-import {MDCTopAppBar} from '@material/top-app-bar/index';
 
-// Instantiation
-const topAppBarElement = document.querySelector('.mdc-toolbar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
+  var elem = document.querySelector('.sidenav');
+  var instance = M.Sidenav.init(elem, options);
+
+  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+  // var collapsibleElem = document.querySelector('.collapsible');
+  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+  // Or with jQuery
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
