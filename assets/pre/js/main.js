0645b2a8f3b6cf4051e76644ec195427f8278f7e
Peter Lee <peterlee0127@gmail.com>
2018-05-02 14:38:05 +0800

    update

--- assets/pre/js/main.js
+++ assets/pre/js/main.js
@@ -1,12 +1,8 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
 
-  var elem = document.querySelector('.sidenav');
-  var instance = M.Sidenav.init(elem);
 
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-  // Or with jQuery
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
