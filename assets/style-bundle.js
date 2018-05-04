cc0751a2385c58c4f4f4cf2c88fe2fbf393c37dd
劉士豪 <liushihao@liushihaode-MacBook-Pro.local>
2018-05-04 21:21:02 +0800

    fix card image and sidebaricon color

--- assets/style-bundle.js
+++ assets/style-bundle.js
@@ -67,15 +67,7 @@
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(1);
-
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports, __webpack_require__) {
-
 module.exports = __webpack_require__.p + "./assets/css/bundle.css";
 
 /***/ })
 /******/ ]);
-//# sourceMappingURL=style-bundle.js.map
