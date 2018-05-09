28e5f9e075d8513e945e694b9cc5cbaa561ed1f1
Peter Lee <peterlee0127@gmail.com>
2018-05-09 23:50:59 +0800

    add bundle.js

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -22468,11 +22468,6 @@ var ScrollSpy = function ScrollSpy() {
     var navHeight = document.querySelector('.navbar-fixed').clientHeight;
     function handleScroll() {
         var currentViewPosition = document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop;
-        var mobileMode = window.matchMedia("(max-width : 600px)");
-        if (mobileMode.matches) {
-            currentViewPosition += navHeight;
-        }
-
         for (var i in elements) {
             var element = elements[i];
             var elementPosition = getPositionOfElement(element.domElement);
