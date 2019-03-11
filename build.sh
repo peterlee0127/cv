03fd10301144842344c42b2b0c93bc710fec7ade
PeterLee <peterlee0127@gmail.com>
2019-03-12 00:09:59 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bundle install
+gem install jekyll html-proofer
 bundle exec jekyll build
-bundle exec htmlproofer ./_site
+bundle exec htmlproofer ./_site --disable-external
