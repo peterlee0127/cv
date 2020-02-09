a106c62f94cf53bf85ec201969756756d7758876
PeterLee <peterlee0127@gmail.com>
2020-02-09 16:18:46 +0800

    Try to fix bundle error, change npm to yarn

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e # halt script on error
-
+yarn install
 bundle install
 JEKYLL_ENV=production bundle exec jekyll build
 #bundle exec htmlproofer ./_site --disable-external
